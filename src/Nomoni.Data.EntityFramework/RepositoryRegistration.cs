3df3e7bf86444747cf5f284375e7e326ad31f020
Jon Ryan <jon@treefish.uk>
2019-04-24 22:08:03 +0100

    Added Logging and Ignored Abstract Classes For Repositories

--- src/Nomoni.Data.EntityFramework/RepositoryRegistration.cs
+++ src/Nomoni.Data.EntityFramework/RepositoryRegistration.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Nomoni.Core.Abstractions;
 using Nomoni.Core.Helpers;
 using Nomoni.Data.Abstractions;
@@ -8,17 +9,30 @@ namespace Nomoni.Data.EntityFramework
 {
     public class RepositoryRegistration : IConfigureServicesAction
     {
+        readonly ILogger log;
+
+        public RepositoryRegistration(ILogger<RepositoryRegistration> logger)
+        {
+            log = logger;
+        }
+
         public int Priority => 100;
 
         public void Execute(IServiceCollection serviceCollection)
         {
-            var typesThatImplementIRepository = AssemblyResolution.GetTypes<IRepository>().Where(x => !x.IsInterface);
+            var typesThatImplementIRepository = AssemblyResolution.GetTypes<IRepository>().Where(x => !x.IsInterface && !x.IsAbstract);
 
             foreach(var type in typesThatImplementIRepository)
             {
                 var interfaceToRegister = type.GetInterfaces().Where(x => x.Name.EndsWith("Repository") && !x.Name.Equals("IRepository")).FirstOrDefault();
 
-                serviceCollection.Add(new ServiceDescriptor(interfaceToRegister, type, ServiceLifetime.Scoped));
+                if(interfaceToRegister != null)
+                {
+                    log.LogInformation("Registered: " + interfaceToRegister.Name + " - " + type.Name);
+
+                    serviceCollection.Add(new ServiceDescriptor(interfaceToRegister, type, ServiceLifetime.Scoped));
+                }
+
             }
         }
     }
